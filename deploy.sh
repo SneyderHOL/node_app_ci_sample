a64ace6611b95bd0e310c9415759b6669f4f9b6b
Sneyder Amador <eduamva@gmail.com>
2025-07-03 10:51:37 -0500

    add staging and prod deploy to workflow

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
-ssh -o StrictHostKeyChecking=no root@200.123.123.12 touch <<HTML
-    cd /var/www/masterclass-actions
+BRANCH=$1
 
-    git pull --rebase origin main
+if [ "$BRANCH" == "main" ]; then
+    DEPLOY_PATH="/home/azureuser/node_app_ci_sample"
+else
+    DEPLOY_PATH="/home/azureuser/staging"
+fi
+
+ssh -o StrictHostKeyChecking=no root@200.123.123.12 touch <<HTML
+    cd $DEPLOY_PATH
+
+    git pull --rebase origin $BRANCH
 HTML
